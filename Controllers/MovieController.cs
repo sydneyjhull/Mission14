0d017ff4421a7232e542c3a43a33b61009ce5fb3
Sydney Hull <sydneyjhull@gmail.com>
2023-04-07 15:27:10 -0600

    Add project files.

---
+++ Controllers/MovieController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Mission14API.Data;
+
+namespace Mission14API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class MovieController : Controller
+    {
+        private MovieDbContext context;
+        public MovieController(MovieDbContext temp) { 
+            context = temp;
+        }
+        public IEnumerable<Movie> Get()
+        {
+            var x = context.Movies.ToArray();
+
+            return context.Movies.ToArray();
+        }
+    }
+}
---
+++ Data/MovieDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Mission14API.Data
+{
+    public class MovieDbContext : DbContext
+    {
+        public MovieDbContext (DbContextOptions<MovieDbContext> options) : base(options) { }
+
+        public DbSet<Movie> Movies { get; set; }
+    }
+}
